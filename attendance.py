01957585c9361d82bf417530761d9ae820a0b80b
Saurabh <saurabh@38960>
2020-04-12 21:44:36 +0530

    added base class

---
+++ attendance.py
@@ -0,0 +1,7 @@
+class AttendanceSystem(object):
+    """docstring forAttendanceSystem."""
+
+    def __init__(self, arg):
+        superAttendanceSystem, self).__init__()
+        self.arg = arg
+        
