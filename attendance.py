db2bf5063c14688c574bb443d67827a7efa6a868
Saurabh <saurabh@38960>
2020-04-12 21:59:07 +0530

    Added modules

--- attendance.py
+++ attendance.py
@@ -1,11 +1,67 @@
-class AttendanceSystem(object):
-    """docstring forAttendanceSystem."""
+import datetime
 
-    def __init__(self, arg):
-        superAttendanceSystem, self).__init__()
-        self.arg = arg
+class AttendanceSystem:
+
+    def __init__(self):
+        self.input_handler = InputHandler()
+        self.database_handler = DatabaseHandler()
+        self.report_generator = ReportGenerator()
+
+    def mark_attendance(self, name):
+        if name in self.database_handler.db.keys():
+            name, time = self.input_handler.mark_attendance(name)
+            self.database_handler.db[name].append(time)
+            print("entry marked")
+
+        else:
+            print("User not found")
+class InputHandler:
+
+    def __init__(self):
+        pass
+    def mark_attendance(self,name):
+        current_time = datetime.datetime.today()
+        return name, current_time
 
 
-    def some_method():
-        print(123)
-        print(456)
+class DatabaseHandler:
+
+    def __init__(self):
+        self.db = {}
+        '''
+        db = {"name":[timestamps]}
+        '''
+
+    def add_user(self, name ):
+        if name in self.db.keys():
+            print("user already exists")
+        else:
+            self.db[name] = []
+
+    def update_user(self):
+        pass
+
+    def delete_user(self):
+        pass
+
+    def get_data(self):
+        pass
+
+class ReportGenerator:
+
+    def __init__(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    system = AttendanceSystem()
+    system.database_handler.add_user("Saurabh")
+    system.database_handler.add_user("Pavan")
+    print(system.database_handler.db)
+    system.mark_attendance("Saurabh")
+    system.mark_attendance("Saurab")
+    system.mark_attendance("saurabh")
+    system.mark_attendance("Pavan")
+    system.mark_attendance("Pavan")
+    print(system.database_handler.db)
